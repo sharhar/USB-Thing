e21bb99c63b93aadd3beb7f6251f189593c6ad41
sharhar <wiishahar@gmail.com>
2016-01-17 11:39:17 -0800

    some code

---
+++ UpdaterFiles/update.py
@@ -0,0 +1,6 @@
+import os
+import zipfile
+import urllib
+
+while(True):
+	print("hello")
---
+++ update.bat
@@ -0,0 +1,4 @@
+@echo off
+"UpdaterFiles\Lib\python-3.5.1.amd64\python.exe" UpdaterFiles\update.py
+echo "Update has finished!"
+pause
