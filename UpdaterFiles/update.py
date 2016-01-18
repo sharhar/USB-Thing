fb910d22a2dbe68ba3d0b7cdc4313c0d07b61479
sharhar <wiishahar@gmail.com>
2016-01-18 08:07:40 -0800

    working code!

--- UpdaterFiles/update.py
+++ UpdaterFiles/update.py
@@ -1,6 +1,18 @@
 import os
 import zipfile
-import urllib
+import urllib.request
 
-while(True):
-	print("hello")
+f = open('UpdaterFiles/serverData.txt','r')
+text = f.read()
+adress = text.split('\n')[0]
+name = text.split('\n')[1]
+urllib.request.urlretrieve(adress, name)
+
+fh = open(name, 'rb')
+z = zipfile.ZipFile(fh)
+for n in z.namelist():
+    outpath = os.getcwd()
+    z.extract(n, outpath)
+fh.close()
+
+os.remove(name)
